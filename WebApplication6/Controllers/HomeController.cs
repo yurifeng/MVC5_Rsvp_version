c42ca08434e200924f8ca3e61baaefcdceb6883a
yurifeng <yuri_feng@outlook.com>
2018-09-07 17:29:19 +0800

    添加项目文件。

---
+++ WebApplication6/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Antlr.Runtime;
+using WebApplication6.Models;
+
+
+namespace WebApplication6.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+
+        public ViewResult index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Message = hour < 12 ? "早上好.." : "下午好...";
+            return View();
+        }
+
+        /// <summary>
+        /// HttpGet提交(用户获取首次的页面是空白的)
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// HttpPost提交(用户填完Form表单提交)
+        /// </summary>
+        /// <param name="gestresponse"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ViewResult RsvpForm(Gestresponse gestresponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", gestresponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication6/Models/Gestresponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication6.Models
+{
+    public class Gestresponse
+    {
+        [Required(ErrorMessage = "请输入姓名")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "请输入电子邮箱地址")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "请输入有效的电子邮箱")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "请输入手机号码")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "请确认是否参加")]
+        public bool? WillAttend { get; set; }
+    }
+}
