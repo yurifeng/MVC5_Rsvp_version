b1093aa5f3d9043c1e59e3f9b4e8407d8fd3e9ec
Yuri Feng <livir_van@outlook.com>
2018-09-09 09:49:56 +0800

    更新组件

--- WebApplication6/Models/Gestresponse.cs
+++ WebApplication6/Models/Gestresponse.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication6.Models
 {
@@ -14,8 +10,11 @@ namespace WebApplication6.Models
         [Required(ErrorMessage = "请输入电子邮箱地址")]
         [RegularExpression(".+\\@.+\\..+", ErrorMessage = "请输入有效的电子邮箱")]
         public string Email { get; set; }
+
         [Required(ErrorMessage = "请输入手机号码")]
+        [RegularExpression("^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\\d{8}$", ErrorMessage = "请输入有效的手机号码")]
         public string Phone { get; set; }
+
         [Required(ErrorMessage = "请确认是否参加")]
         public bool? WillAttend { get; set; }
     }
